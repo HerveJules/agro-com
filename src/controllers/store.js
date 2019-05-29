cd05189344d860d71e325e53fa79ec096deca91a
shemaeric <shemaeric01@gmail.com>
2019-05-29 19:00:28 +0200

    refactor

--- src/controllers/store.js
+++ src/controllers/store.js
@@ -1,36 +1,45 @@
-import db from '../models';
+import db from "../models";
 
-const {Store} = db;
+const { Store, Coop } = db;
 
 // class for store operation controller
 
-class store{
-	// function to add product to store
+class store {
+  // function to add product to store
 
-	static addStore(req,res){
+  static async addStore(req, res) {
+		const { body } = req;
+		const { coopName } = req.params;
 
-		Store.create({...req.body,CoopId:req.params.CoopId}).then(result =>{
-			res.status(200).send({
-				message:'stock updated successfully!',
-				data:{
-					result,
-				}
+		try {
+			const findCooperative = await Coop.findOne({where: { coopName }})
+			if(!findCooperative) {
+				return res.status(400).send({
+					status: res.statusCode,
+					error: 'The cooperative is not found'
+				})
+			}
+
+			const createStore = await Store.create({ body, coopId: findCooperative.id })
+			return res.status(201).send({
+				status: res.statusCode,
+				message: 'the store has successfully been created',
+				data: createStore
 			})
-		}).catch(err =>{
-			res.status(203).send({
-				message:'Something went wrong while updating store',
-				error:err,
+		} catch (error) {
+			return res.status(500).send({
+				status: res.statusCode,
+				error: 'Something went wrong on the server'
 			})
-		});
-	}
+		}
+  }
 
-	// function that is going to update store and publish to auction
+  // function that is going to update store and publish to auction
 
-	static publish(req,res){
-		//find product with tin
-		Store.findById({where:{id}}); 
-		
-	}	
+  static publish(req, res) {
+    //find product with tin
+    Store.findById({ where: { id } });
+  }
 }
 
 export default store;
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,140 +1,141 @@
-import db from '../models';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import {compareHashedPassword, generateHash} from '../helpers';
-import validation from '../middleware/validations';
-import secret from '../config/secretKey.js';
-import jwtpassport from '../config/passport';
-import {cloudinaryConfig, uploader } from '../config/cloudinaryConfig';
-import cloud from '../helpers/clouds';
+import db from "../models";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { compareHashedPassword, generateHash } from "../helpers";
+import validation from "../middleware/validations";
+import secret from "../config/secretKey.js";
+import jwtpassport from "../config/passport";
+import { cloudinaryConfig, uploader } from "../config/cloudinaryConfig";
+import cloud from "../helpers/clouds";
 
-
-const secretKey = secret.secretKey
+const secretKey = secret.secretKey;
 const { User } = db;
-const {sessionData} = jwtpassport;
+const { sessionData } = jwtpassport;
 class Users {
-
-    static async createUser(req, res) {
-        const {
-            firstname, lastname, email, password, role, 
-            adress, tel, ID, jobtitle, image  
-        } = req.body                
-        try {
-            const userFind = await User.findOne({ where: { email}})
-            if(userFind) {
-              return res.status(400).send({
-                status:400,
-                errorMessage: 'The User with that email exists'
-              })
-            }
-            const links = await cloud(req.files);
-            const encryptedPassword = await generateHash(password);
-            const userSave = await User.create({ firstname,lastname,email, 
-              password:encryptedPassword,role,adress,tel, jobtitle, ID,image:links[0]});
-            if(userSave) {
-              return res.status(201).send({
-                status:res.statusCode,
-                message: 'User has been created',
-                user: {
-                  email: userSave.email,
-                  jobtitle: userSave.jobtitle
-                }
-              })
-            }
-        }
-        catch(err) {
-          res.status(500).send({
-            status:res.statusCode,
-            message:err
-          })
-        }
+  static async createUser(req, res) {
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      adress,
+      tel,
+      ID,
+      jobtitle,
+    } = req.body;
+    try {
+      const userFind = await User.findOne({ where: { email } });
+      if (userFind) {
+        return res.status(400).send({
+          status: 400,
+          errorMessage: "The User with that email exists"
+        });
+      }
+      const links = await cloud(req.files);
+      console.log(links);
+      const encryptedPassword = await generateHash(password);
+      const userSave = await User.create({
+        firstname,
+        lastname,
+        email,
+        password: encryptedPassword,
+        adress,
+        tel,
+        jobtitle,
+        ID,
+        image: links[0]
+      }, attributes = ['id']);
+      if (userSave) {
+        return res.status(201).send({
+          status: res.statusCode,
+          message: "User has been created",
+          user: {
+            email: userSave.email,
+            jobtitle: userSave.jobtitle,
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        
+      });
     }
+  }
   // function that do login operationscompareHashedPassword
 
-  static async auth(req,res) {
+  static async auth(req, res) {
     // body...
-    const {email,password} = req.body;
+    const { email, password } = req.body;
 
-    try{
-      const userfindOne = await User.findOne({where:{email}});
-        if (userfindOne) {
-
-          if (compareHashedPassword(password,userfindOne.password)) {
-            const user = {
-              id:userfindOne.id,
-              tin:userfindOne.tin
-            }
-            const token = jwt.sign(user,secretKey);
-            return res.status(201).send({
-              status:201,
-              message:'You have successfully logged in',
-              token
-            })
-          } else {
-            return res.status(401).send({
-              status:401,
-              message:'incorrect password'
-            })
-          }
-          
-        }else{
-          // if no user found by email report incorrect email
-          return res.status(400).send({
-            status:400,
-            message:'User does not exists'
-          })
+    try {
+      const userfindOne = await User.findOne({ where: { email } });
+      if (userfindOne) {
+        if (compareHashedPassword(password, userfindOne.password)) {
+          const user = {
+            id: userfindOne.id,
+            tin: userfindOne.tin
+          };
+          const token = jwt.sign(user, secretKey);
+          return res.status(201).send({
+            status: 201,
+            message: "You have successfully logged in",
+            token
+          });
+        } else {
+          return res.status(401).send({
+            status: 401,
+            message: "incorrect password"
+          });
         }
-    }catch(err){
+      } else {
+        // if no user found by email report incorrect email
+        return res.status(400).send({
+          status: 400,
+          message: "User does not exists"
+        });
+      }
+    } catch (err) {
       res.status(203).send({
-        message:err
-      })
+        message: err
+      });
     }
-    
   }
 
-  static secret (req,res) {
+  static secret(req, res) {
     return res.send({
-      message:'authorized'
-    })
+      message: "authorized"
+    });
   }
-
 
   // upload file
 
-  static uploadFile(req,res){
-     
-
-    if(req.file) {
-
+  static uploadFile(req, res) {
+    if (req.file) {
       const file = dataUri(req).content;
 
-        return uploader.upload(file).then((result) => {
-
+      return uploader
+        .upload(file)
+        .then(result => {
           const image = result.url;
 
           return res.status(200).json({
-
-            messge: 'Your image has been uploded successfully to cloudinary',
+            messge: "Your image has been uploded successfully to cloudinary",
 
             data: {
-
               image
-
             }
-
-          })
-
-        }).catch((err) => res.status(400).json({
-
-            messge: 'someting went wrong while processing your request',
+          });
+        })
+        .catch(err =>
+          res.status(400).json({
+            messge: "someting went wrong while processing your request",
 
             data: {
-
               err
-
             }
-
-        }))
+          })
+        );
     }
   }
 }
