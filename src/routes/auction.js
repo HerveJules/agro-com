5b34765d5e02a23626f110cde167a8e1ea1e45d6
HerveJules <hervejulesiradukunda@gmail.com>
2019-07-04 10:48:15 +0200

    resolve conflict to auction route

--- src/routes/auction.js
+++ src/routes/auction.js
@@ -4,6 +4,6 @@ import passportAuth from '../config/passport';
 import Auction from '../controllers/auction';
 const router = express.Router();
 
-router.post('/api/v1/pub/auction/:StoreId',Auction.addAuction)
+// router.post('/api/v1/pub/auction/:StoreId',Auction.addAuction)
 
 export default router;
